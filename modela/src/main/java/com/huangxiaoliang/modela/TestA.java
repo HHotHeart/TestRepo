9a85c5df1e7351402244cc65317185c42adac638
HHotHeart <2550754753@qq.com>
2022-07-31 23:31:41 +0800

    添加model测试代码

---
+++ modela/src/main/java/com/huangxiaoliang/modela/TestA.java
@@ -0,0 +1,12 @@
+package com.huangxiaoliang.modela;
+
+/**
+ * <pre>@author HHotHeart</pre>
+ * <pre>@date 2022/7/31 23:27</pre>
+ * <pre>@desc 描述</pre>
+ */
+public class TestA {
+    public static String show(){
+        return "modelA";
+    }
+}
---
+++ modelb/src/main/java/com/huangxiaoliang/modelb/TestB.java
@@ -0,0 +1,13 @@
+package com.huangxiaoliang.modelb;
+
+/**
+ * <pre>@author HHotHeart</pre>
+ * <pre>@date 2022/7/31 23:27</pre>
+ * <pre>@desc 描述</pre>
+ */
+public class TestB {
+
+    public static String show(){
+        return "modelB";
+    }
+}
